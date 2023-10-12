0027e976bf3494780d00a91dab455ef5f3339bba
Joao Boemer <joao.boemer@edu.univali.com>
2023-10-12 13:19:57 -0300

    Adicionando telas de luta e trocas de tela ao andar pelo cenário

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -29,9 +29,13 @@ public class MyGdxGame extends ApplicationAdapter {
 	dir d = dir.left;
 	TextureRegion currentFrame;
 	enum Screen {
-		TITLE, MAIN_GAME, GAME_OVER;
+		TITLE, MAIN_GAME, GOBLIN_FIGHT, SKELETON_FIGHT, MINOTAUR_FIGHT, GAME_OVER;
 	}
 	Screen currentScreen = Screen.TITLE;
+
+	private boolean golin_fight = false;
+	private boolean skeleton_fight = false;
+	private boolean minotaur_fight = false;
 
 	@Override
 	public void create () {
@@ -46,18 +50,74 @@ public class MyGdxGame extends ApplicationAdapter {
 				if(currentScreen == Screen.TITLE && keyCode == Input.Keys.SPACE){
 					currentScreen = Screen.MAIN_GAME;
 				}
-				else if(currentScreen == Screen.GAME_OVER && keyCode == Input.Keys.ENTER){
-					currentScreen = Screen.TITLE;
-				}
-				else if( currentScreen == Screen.MAIN_GAME && keyCode == Input.Keys.ESCAPE )
-				{
-					currentScreen = Screen.TITLE;
-				}
 
 				return true;
 			}
 		});
 
+		main_game_create();
+	}
+
+	@Override
+	public void render () {
+
+		switch ( currentScreen )
+		{
+			case TITLE:
+				title_render();
+				break;
+			case MAIN_GAME:
+				main_game_render();
+				break;
+			case GOBLIN_FIGHT:
+				goblin_fight_render();
+				break;
+			case SKELETON_FIGHT:
+				skeleton_fight_render();
+				break;
+			case MINOTAUR_FIGHT:
+				minotaur_fight_render();
+				break;
+			case GAME_OVER:
+				game_over_render();
+				break;
+		}
+	}
+
+	public void game_over_render()
+	{
+		batch.begin();
+		batch.draw(tBackground, 0, 0);
+		batch.end();
+	}
+
+	public void title_render()
+	{
+		batch.begin();
+		batch.draw(tBackground, 0, 0);
+		batch.end();
+	}
+
+	public void goblin_fight_render()
+	{
+		batch.begin();
+		batch.draw(tBackground, 0, 0);
+		batch.end();
+	}
+	public void skeleton_fight_render()
+	{
+		batch.begin();
+		batch.draw(tBackground, 0, 0);
+		batch.end();
+	}
+	public void minotaur_fight_render()
+	{
+		batch.begin();
+		batch.draw(tBackground, 0, 0);
+		batch.end();
+	}
+	public void main_game_create()
+	{
 		TextureRegion[][] tmp = TextureRegion.split(tWalkingCharacter,
 				tWalkingCharacter.getWidth() / 3,
 				tWalkingCharacter.getHeight() / 4);
@@ -93,61 +153,58 @@ public class MyGdxGame extends ApplicationAdapter {
 
 		idleAnimation = new Animation<TextureRegion>(FRAME_TIME, tmp[3][1]);
 
-		posX = 600;
+		posX = 0;
 		posY = 300;
 	}
+	public void main_game_render()
+	{
+		this.move();
+		elapsed_time += Gdx.graphics.getDeltaTime();
 
-	@Override
-	public void render () {
-
-		if ( currentScreen == Screen.MAIN_GAME )
+		if ( walking )
 		{
-			this.move();
-			elapsed_time += Gdx.graphics.getDeltaTime();
-
-			if ( walking )
+			switch (d)
 			{
-				switch (d)
-				{
-					case up:
-						currentFrame = runningUpAnimation.getKeyFrame(elapsed_time, true);
-						break;
-					case down:
-						currentFrame = runningDownAnimation.getKeyFrame(elapsed_time, true);
-						break;
-					case left:
-						currentFrame = runningLeftAnimation.getKeyFrame(elapsed_time, true);
-						break;
-					case right:
-						currentFrame = runningRightAnimation.getKeyFrame(elapsed_time, true);
-						break;
-				}
-			}
-			else
-			{
-				currentFrame = idleAnimation.getKeyFrame(elapsed_time, true);
-			}
-
-			ScreenUtils.clear(1, 0, 0, 1);
-			batch.begin();
-			batch.draw(tBackground, 0, 0);
-			batch.draw(currentFrame, posX, posY);
-			batch.end();
-			if ( posX == 0 && posY == 0 )
-			{
-				currentScreen = Screen.TITLE;
+				case up:
+					currentFrame = runningUpAnimation.getKeyFrame(elapsed_time, true);
+					break;
+				case down:
+					currentFrame = runningDownAnimation.getKeyFrame(elapsed_time, true);
+					break;
+				case left:
+					currentFrame = runningLeftAnimation.getKeyFrame(elapsed_time, true);
+					break;
+				case right:
+					currentFrame = runningRightAnimation.getKeyFrame(elapsed_time, true);
+					break;
 			}
 		}
-		else if ( currentScreen == Screen.TITLE )
+		else
 		{
-			batch.begin();
-			batch.draw(tBackground, 0, 0);
-			//batch.draw(currentFrame, posX, posY);
-			batch.end();
+			currentFrame = idleAnimation.getKeyFrame(elapsed_time, true);
 		}
 
+		ScreenUtils.clear(1, 0, 0, 1);
+		batch.begin();
+		batch.draw(tBackground, 0, 0);
+		batch.draw(currentFrame, posX, posY);
+		batch.end();
+
+		if ( posX >= 300 && !golin_fight )
+		{
+			currentScreen = Screen.GOBLIN_FIGHT;
+		}
+
+		if ( posX >= 600 && !skeleton_fight )
+		{
+			currentScreen = Screen.SKELETON_FIGHT;
+		}
+
+		if ( posX >= 1000 && !minotaur_fight )
+		{
+			currentScreen = Screen.MINOTAUR_FIGHT;
+		}
 	}
-	
 	@Override
 	public void dispose () {
 		batch.dispose();
